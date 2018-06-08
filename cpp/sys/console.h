c4af4919665d66bc0a9f56a0384139868dd87f87
litikz dev <35007085+Rappaaa@users.noreply.github.com>
2018-06-08 22:00:15 +0200

    sources are now two differents branches cpp, lts

---
+++ cpp/sys/console.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+class console{
+	public:
+		const char* println_arg;
+		void println();
+		const char* print_arg;
+		void print();
+};
---
+++ cpp/sys/file.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class file{
+	public:
+		char* file_bytes;
+		const char* path;
+		unsigned int length;
+		void init_file();
+		unsigned int i;
+		char temp;
+};
